2a2ba121fd04a9b8c6fbd93d3dd9b3c74e255720
¨Chris <crberko@gmail.com>
2013-04-08 19:08:19 -0500

    finish 8

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 SampleApp::Application.routes.draw do
   # get "users/new"
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   #get "static_pages/home"
   #match '/home', to: 'static_pages#home'
   root to: 'static_pages#home'
 
   match '/signup', to: 'users#new'  
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
   #get "static_pages/help"
   match '/help', to: 'static_pages#help'
 
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,12 +5,12 @@ describe "User Pages" do
  	 subject { page }
 
  	 describe "profile page" do
-  let(:user) { FactoryGirl.create(:user) }
-    before { visit user_path(user) }
+      let(:user) { FactoryGirl.create(:user) }
+      before { visit user_path(user) }
     
-  it { should have_selector('h1',    text: user.name) }
-  it { should have_selector('title', text: user.name) }
-end
+      it { should have_selector('h1',    text: user.name) }
+      it { should have_selector('title', text: user.name) }
+    end
 
 
  	 describe "signup page" do
@@ -18,15 +18,15 @@ end
 
    		it { should have_selector('h1',    text: 'Sign up') }
     	it { should have_selector('title', text: 'Sign up') }
-
+    
     	 describe "signup" do
 
-    before { visit signup_path }
+      before { visit signup_path }
 
-    let(:submit) { "Create my account" }
+      let(:submit) { "Create my account" }
 
-    describe "with invalid information" do
-      it "should not create a user" do
+      describe "with invalid information" do
+          it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
     end
@@ -43,5 +43,6 @@ end
         expect { click_button submit }.to change(User, :count).by(1)
       end
     end
-  	end
+  end
+end
 end
